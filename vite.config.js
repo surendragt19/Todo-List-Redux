83e311a8078bc98f67e8e8344e8dfd1ceb6c7be9
Surendra Kumar Gupta <100050291+surendragt19@users.noreply.github.com>
2024-06-14 20:03:29 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-icons/rx'],
+    },
+  },
 })
